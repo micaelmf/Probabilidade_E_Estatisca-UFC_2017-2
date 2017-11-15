749cc737be45a52eb571d207084d2385f46fb2b3
Micael Ferreira <micaelmf@gmail.com>
2017-11-15 11:36:56 -0300

    Primeiro Commit - Carregando arquivos, escolhendo o primeiro grupo e somando.

--- src/Principal.java
+++ src/Principal.java
@@ -6,16 +6,29 @@ import javax.swing.plaf.synth.SynthSeparatorUI;
 public class Principal {
 
 	public static void main(String[] args) throws IOException {
-		Problema01 p = new Problema01(3,7); //(quantidade de cargos, quantidade de funcionários)
-
-		System.out.println("\n----------- DISTANCIAS -----------");
+		Problema01 p = new Problema01(3,45); //(quantidade de cargos, quantidade de funcionários)
+		
+		//p.rand(1, 70, 44);
+		
+		//System.out.println("\n----------- DISTANCIAS -----------");
 		p.carregaDistancias();
-		p.imprimiDistancias();
-		System.out.println("\n----------- CARGOS -----------");
+		//p.imprimiDistancias();
+		//System.out.println("\n----------- CARGOS -----------");
 		p.carregaCargos();
-		p.imprimiCargos();
-
-
+		//p.imprimiCargos();
+		
+		//total de cada cargo na organização
+		p.setTotalEnfermeiros(21);
+		p.setTotalMedicos(10);
+		p.setTotalSecretarios(13);
+		//divisão da quantidade de cada cargo por grupo
+		p.setMaxEnfermeiros(8);
+		p.setMaxMedicos(2);
+		p.setMaxSecretarios(5);
+		
+		p.primeiroGrupo();
+		
+		
 	}
 
 }
--- src/Problema01.java
+++ src/Problema01.java
@@ -3,16 +3,26 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.regex.Pattern;
+
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
 import java.util.Scanner;
 
 
 public class Problema01 {
-	private ArrayList combinacao = new ArrayList();
-	private ArrayList resultado = new ArrayList();
-	private int qntFunc = 0;
-	private int qntCargos = 0;
+	private int qntFunc = 0; //quantos funcionários existem na matriz
+	private int qntCargos = 0; //quantos cargos diferentes existem na matriz
+	private int totalMedicos = 0; //quantidade total de médicos
+	private int totalEnfermeiros = 0; //quantidade total de enfermeiros
+	private int totalSecretarios = 0; //quantidade total de secretários
+	private int maxMedicos = 0; //quantidade mínima que um grupo deve ter de médicos
+	private int maxEnfermeiros = 0; //quantidade mínima que um grupo deve ter de enfermeiros
+	private int maxSecretarios = 0; //quantidade mínima que um grupo deve ter de sercretários
 	private int distancias[][] = null;
 	private int cargos[][] = null;
+	private int melhorSoma = 0;
+	private ArrayList<Integer> melhorGrupo = new ArrayList<Integer>();
+	private ArrayList<Integer> grupo = new ArrayList<Integer>();
 	
 	Problema01(int qntCargos, int qntFunc){
 		this.qntCargos = qntCargos;
@@ -24,8 +34,111 @@ public class Problema01 {
 		this.cargos = matrizCargos;
 	}
 	
+	public void primeiroGrupo(){
+		//System.out.println(this.maxEnfermeiros);
+		int contM = 0;
+		int contE = 0;
+		int contS = 0;
+		ArrayList<Integer> posicoes = new ArrayList<Integer>();
+		
+		for(int i = 0; i < qntFunc; i++){
+			if(this.cargos[i][0] == 1){					
+				//É MÉDICO
+				if(contM < this.maxMedicos){
+					//System.out.printf("méd("+ i +")|");
+					posicoes.add(i); //identifica o índice do funcionário na matriz de cargos
+				}
+				contM++;
+			}else if(this.cargos[i][1] == 1){
+				//É ENFERMEIRO
+				if(contE < this.maxEnfermeiros){
+					//System.out.printf("enf("+ i +")|");
+					posicoes.add(i);
+				}
+				contE++;
+			}else if(this.cargos[i][2] == 1){
+				//É SECRETÁRIO
+				if(contS < this.maxSecretarios){
+					//System.out.printf("sec("+ i +")|");
+					posicoes.add(i);
+				}
+				contS++;
+			}
+		}
+		System.out.printf("1º Grupo:    " );
+		atribuiDistancias(posicoes);
+		imprimiGrupo(this.grupo);
+		somaGrupo();
+		this.melhorGrupo = this.grupo;
+	}
 	
+	public void trocaGrupo(){
+		trocaMedico();
+	}
 	
+	public void trocaMedico(){
+		
+	}
+	
+	public String encontraCargo(){
+		for(int i = 0; i < qntFunc; i++){
+			if(this.cargos[i][0] == 1){					
+				return "medico";
+			}else if(this.cargos[i][1] == 1){
+				return "emfermeiro";
+			}else if(this.cargos[i][2] == 1){
+				return "secretario";
+			}
+		}
+		return null;
+	}
+	
+	public void atribuiDistancias(ArrayList<Integer> posicoes){
+		imprimiGrupo(posicoes);
+		//atribuindo ao arraylist grupo as distancias entre os funcionários
+		for(int i = 0; i <= posicoes.size(); i++){
+			for(int j = i+1; j < posicoes.size(); j++){
+				this.grupo.add(this.distancias[posicoes.get(i)][posicoes.get(j)]);
+				break;
+			}
+		}
+		this.grupo.add(this.distancias[posicoes.get(0)][posicoes.get((calcTamGrupo())-1)]);
+		
+		System.out.printf("Grupo(Dist): ");
+	}
+	
+	public int somaGrupo(){
+		int soma = 0;
+		int parcial = 0;
+		for (int i = 0; i < this.grupo.size(); i++) {
+			parcial = 0;
+			for (int j = i+1; j < this.grupo.size(); j++) {
+				parcial = parcial + this.grupo.get(i) + this.grupo.get(j);				
+				System.out.printf("(" + this.grupo.get(i) + " + " + this.grupo.get(j) + ")");
+			}
+			soma = soma + parcial;
+			if(parcial != 0){
+				System.out.println(" Parcial: " + parcial);
+			}
+		}
+		
+		if(soma > this.melhorSoma){
+			//chamar o método que troca o grupo
+		}else{
+			
+		}
+		System.out.println("_________________________");
+		System.out.println("RESULTADO: " + soma);
+		
+		return soma;
+	}
+	
+	public void imprimiGrupo(ArrayList<Integer> grupo){
+		for(int i = 0; i < grupo.size(); i++){
+			System.out.printf(grupo.get(i) + "|");
+		}
+		System.out.println("");
+	}
 	
 	public void carregaDistancias() throws IOException{
 		
@@ -72,6 +185,7 @@ public class Problema01 {
         }
 	
 	}
+	
 	public void imprimiDistancias(){
 		for(int i = 0; i < this.distancias.length; i++){
         	for(int j = 0; j < this.distancias.length; j++){
@@ -89,7 +203,90 @@ public class Problema01 {
         	System.out.println("");
         }
 	}
+	
+	public void rand(int Str, int End, int tam) {
+		ArrayList<Integer> numeros = new ArrayList<Integer>();
+		boolean add = false;
+		int num = (int) Math.ceil(Math.random() * (End  - Str + 1)) - 1 + Str;
+		numeros.add(num);
+		for(int i = tam; i > 0; i--){
+			while (numeros.size() <= i){
+				num = (int) Math.ceil(Math.random() * (End  - Str + 1)) - 1 + Str;
+				for(int j = 0; j < numeros.size(); j++){
+					if (num != numeros.get(j)){
+						add = true;
+					}else{
+						add = false;
+						break;
+					}
+				}
+				if(add == true){
+					numeros.add(num);
+					System.out.printf(num + "|");
+				}
+			}
+			System.out.println("");
+			numeros = new ArrayList<Integer>();
+			num = (int) Math.ceil(Math.random() * (End  - Str + 1)) - 1 + Str;
+			numeros.add(num);
+		}
+		
+        
+    }
+	
+	public int calcTamGrupo(){
+		return this.getMaxEnfermeiros() + this.getMaxMedicos() + this.getMaxSecretarios();
+	}
+	
+	public int getTotalMedicos() {
+		return totalMedicos;
+	}
 
+	public void setTotalMedicos(int totalMedicos) {
+		this.totalMedicos = totalMedicos;
+	}
+
+	public int getTotalEnfermeiros() {
+		return totalEnfermeiros;
+	}
+
+	public void setTotalEnfermeiros(int totalEnfermeiros) {
+		this.totalEnfermeiros = totalEnfermeiros;
+	}
+
+	public int getTotalSecretarios() {
+		return totalSecretarios;
+	}
+
+	public void setTotalSecretarios(int totalSecretarios) {
+		this.totalSecretarios = totalSecretarios;
+	}
+
+	public int getMaxMedicos() {
+		return maxMedicos;
+	}
+
+	public void setMaxMedicos(int maxMedicos) {
+		this.maxMedicos = maxMedicos;
+	}
+
+	public int getMaxEnfermeiros() {
+		return maxEnfermeiros;
+	}
+
+	public void setMaxEnfermeiros(int maxEnfermeiros) {
+		this.maxEnfermeiros = maxEnfermeiros;
+	}
+
+	public int getMaxSecretarios() {
+		return maxSecretarios;
+	}
+
+	public void setMaxSecretarios(int maxSecretarios) {
+		this.maxSecretarios = maxSecretarios;
+	}
+
+	
 	
 
 }
